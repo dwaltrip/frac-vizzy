21da8c3345123a78fea07ea746ce7190306edd2b
Daniel Waltrip <dwaltrip77@gmail.com>
2022-09-21 14:10:55 -0700

    Helper script for installing backend deps

--- backend/tools/install-new-dependency.sh
+++ backend/tools/install-new-dependency.sh
@@ -6,8 +6,7 @@ DJANGO_DIR="$BACKEND_DIR/project"
 
 cd "$BACKEND_DIR"
 source "$BACKEND_DIR/venv/bin/activate"
-cd "$DJANGO_DIR"
 
-# Allow optionally passing a command, like so:
-#   bash tools/venv-runner.sh do_something arg1 --arg2
-${@:1}
+pip-compile "$BACKEND_DIR/requirements.in"
+
+pip install -r "$BACKEND_DIR/requirements.txt"
--- backend/tools/start-dev-server.sh
+++ backend/tools/start-dev-server.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-ROOT_DIR="$(dirname "$DIR")"
-DJANGO_DIR="$ROOT_DIR/project"
+BACKEND_DIR="$(dirname "$DIR")"
+DJANGO_DIR="$BACKEND_DIR/project"
 
-cd "$ROOT_DIR"
-source "$ROOT_DIR/venv/bin/activate"
+cd "$BACKEND_DIR"
+source "$BACKEND_DIR/venv/bin/activate"
 cd "$DJANGO_DIR"
 
 python manage.py runserver
