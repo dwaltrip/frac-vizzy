2747766729280e1b154ab18149856119fd989dcc
Daniel Waltrip <dwaltrip77@gmail.com>
2022-09-29 10:01:55 -0700

    Setup basic routing (react-router). Add dummy Home route.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,32 @@
 import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import './styles/App.css';
 
 import { FractalExplorer } from './FractalExplorer';
 
-export default function App() {
+const DummyHomePage = () => (
+  <h3>Frac Vizzy Home Page</h3>
+);
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <DummyHomePage />
+  },
+  {
+    path: '/explore',
+    element: <FractalExplorer />
+  },
+]);
+
+function App() {
+
   return (
     <div className='App'>
-      <FractalExplorer />
+      <RouterProvider router={router} />
     </div>
   );
-};
+}
+
+export default App;
