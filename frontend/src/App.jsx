f8e002f8e0ea2c89e621483c2bd07bf7b3c170a3
Daniel Waltrip <dwaltrip77@gmail.com>
2022-09-29 08:51:09 -0700

    Moving fractal stuff out of App in prep for multiple pages
    Create FractalExplorer to contain all that fractal stuff.
    Note: FractalExplorer is mostly just a rename of App.jsx, and
      has very few changes. But git is too dumb to understand so it
      shows many changes.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,87 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import qs from 'qs';
+import React from 'react';
 
 import './styles/App.css';
 
-import { getInitialParams, serializeParams, normalizeParams } from './plotParams';
-import { getInitialSystemParams, saveSystemParams } from './systemParams';
-import { getViewportInfo } from './viewport';
-import { getInitialZoomLevel } from './mandelbrot/calcs';
+import { FractalExplorer } from './FractalExplorer';
 
-import { SettingsPanel } from './SettingsPanel';
-import { MandelbrotPlot } from './MandelbrotPlot';
-
-let userHasChangedParams = false;
-
-function App() {
-  console.log('=== Rendering App... ==='); 
-  const [viewportRect, setViewportRect] = useState(null);
-  const [plotParams, setParamsRaw] = useState(getInitialParams());
-  const [systemParams, setSystemParamsRaw] = useState(getInitialSystemParams());
-
-  function setPlotParams({ ...newParams }) {
-    // We are relying on the fact that `setPlotParams` currently only gets
-    // called when the user takes an action that changes the params.
-    // Either via a canvas event or by the settings panel.
-    // This is slighlty brittle...
-    userHasChangedParams = true;
-    const viewport = getViewportInfo(plotParams, viewportRect);
-    setParamsRaw(prevParams => normalizeParams(
-      { ...prevParams, ...newParams },
-      viewport,
-    ));
-  }
-
-  useEffect(() => {
-    const isZoomLevelNotSetAndWeHaveViewportSize = (
-      !plotParams.zoomLevel && viewportRect
-    );
-    if (isZoomLevelNotSetAndWeHaveViewportSize) {
-      setParamsRaw(prevParams => ({
-        ...prevParams,
-        zoomLevel: getInitialZoomLevel(viewportRect),
-      }));
-    }
-  }, [viewportRect]);
-
-  function setSystemParams({ ...newParams }) {
-    setSystemParamsRaw(prevParams => ({ ...prevParams, ...newParams }));
-  }
-
-  useEffect(() => {
-    // If the URL doesn't have any params, don't add them until the user makes
-    // a change. This prevents the URL from being cluttered when the user opens
-    // the app for the first time and hasn't yet zoomed in on anything.
-    if (!userHasChangedParams) {
-      return;
-    }
-    const relPathWithQuery = (
-      window.location.pathname + '?' +
-      qs.stringify(serializeParams(plotParams), { encode: false })
-    );
-    window.history.replaceState(null, '', relPathWithQuery);
-  }, [plotParams]);
-
-  useEffect(() => saveSystemParams(systemParams), [systemParams]);
-
+export default function App() {
   return (
     <div className='App'>
-      <SettingsPanel
-        params={plotParams}
-        setPlotParams={setPlotParams}
-        systemParams={systemParams}
-        setSystemParams={setSystemParams}
-      />
-
-      <MandelbrotPlot
-        params={plotParams}
-        setPlotParams={setPlotParams}
-        systemParams={systemParams}
-        viewportRect={viewportRect}
-        setViewportRect={setViewportRect}
-      />
+      <FractalExplorer />
     </div>
   );
-}
-
-export default App;
+};
