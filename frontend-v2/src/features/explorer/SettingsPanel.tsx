91ffa708807168074aa2e05a0ec9e5fa5217d690
Daniel Waltrip <dwaltrip77@gmail.com>
2024-11-22 15:45:46 -0800

    [wip] Show the panel if user mouses over left edge of screen

--- frontend-v2/src/features/explorer/SettingsPanel.tsx
+++ frontend-v2/src/features/explorer/SettingsPanel.tsx
@@ -1,7 +1,36 @@
 import '@/styles/features/explorer/settings-panel.css';
+import { useState } from 'react';
 
 function SettingsPanel() {
-  return <div className='settings-panel'>Settings Panel</div>;
+  return (
+    <div className='settings-panel'>
+      <SlideOutPanel>
+        <div className='settings-panel-content'>Settings Panel</div>
+      </SlideOutPanel>
+    </div>
+  );
+}
+
+function SlideOutPanel({ children }: { children: React.ReactNode }) {
+  const [isOpen, setIsOpen] = useState(true);
+
+  return (
+    <div className={`slide-out-panel-container ${isOpen && 'visible'}`}>
+      {isOpen && (
+        <div
+          className='slide-out-panel-content'
+          onMouseLeave={() => setIsOpen(false)}
+        >
+          {children}
+        </div>
+      )}
+      {/* <div className='slide-out-panel-opener'></div> */}
+      <div
+        className='slide-out-panel-opener'
+        onMouseEnter={() => setIsOpen(true)}
+      ></div>
+    </div>
+  );
 }
 
 export { SettingsPanel };
