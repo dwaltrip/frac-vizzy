5e7b07b92c683b6f35fa53041f6da259cba53ed3
Daniel Waltrip <dwaltrip77@gmail.com>
2024-11-25 17:58:04 -0800

    Make SlideOutPanel more generic and add simple animation

--- frontend-v2/src/features/explorer/SettingsPanel.tsx
+++ frontend-v2/src/features/explorer/SettingsPanel.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import { ColorPicker } from '@/ui/ColorPicker';
 
 import '@/styles/features/explorer/settings-panel.css';
+import classnames from 'classnames';
 
 const ITERATION_VALUE_OPTS = [100, 250, 500, 1000, 2500, 5000].map((num) => {
   const value = '' + num;
@@ -35,13 +36,18 @@ function SettingsRow({
   );
 }
 
+// ----------------------------------------------------------------------
+// TODO: Need to add some sort of affordance so the user realizes how to
+// access the settings panel. Right now it's pretty "hidden" / non-discoverable
+// ----------------------------------------------------------------------
+
 function SettingsPanel({
   onItersChange,
 }: {
   onItersChange: (iters: number) => void;
 }) {
   // ---------------------------------------------------------------
-  // TODO: these are placeholdders for now so the code can run
+  // TODO: these are placeholders for now so the code can run
   // Will implement the actual functionality later
   const [color1, setColor1] = useState({ r: 70, g: 70, b: 70 });
   const [color2, setColor2] = useState({ r: 255, g: 255, b: 255 });
@@ -58,71 +64,67 @@ function SettingsPanel({
   console.log('iters:', iters);
 
   return (
-    // TODO: div.settings-panel should be inside of <SlideOutPanel>
-    // Need to adjust the styles accordingly
-    <div className='settings-panel'>
-      <SlideOutPanel>
-        {/* TODO: decide if I want to use this or not */}
-        {/* {({ isOpen, setIsOpen }) => ( */}
-        {() => (
-          <div className='settings-panel-content'>
-            <div className='settings-panel-header'>Frac Vizzy</div>
-            <div className='settings-rows-container'>
-              <SettingsRow label='Iterations'>
-                <select
-                  className='settings-select'
-                  value={iters}
-                  onChange={setIters}
-                >
-                  {ITERATION_VALUE_OPTS.map(({ value, text }) => (
-                    <option value={value} key={value}>
-                      {text}
-                    </option>
-                  ))}
-                </select>
-              </SettingsRow>
+    <SlideOutPanel>
+      {/* TODO: decide if I want to use this or not */}
+      {/* {({ isOpen, setIsOpen }) => ( */}
+      {() => (
+        <div className='settings-panel'>
+          <div className='settings-panel-header'>Frac Vizzy</div>
+          <div className='settings-rows-container'>
+            <SettingsRow label='Iterations'>
+              <select
+                className='settings-select'
+                value={iters}
+                onChange={setIters}
+              >
+                {ITERATION_VALUE_OPTS.map(({ value, text }) => (
+                  <option value={value} key={value}>
+                    {text}
+                  </option>
+                ))}
+              </select>
+            </SettingsRow>
 
-              <SettingsRow label='Color'>
-                <ColorPicker color={color1} onChange={setColor1} />
-                <ColorPicker color={color2} onChange={setColor2} />
-              </SettingsRow>
+            <SettingsRow label='Color'>
+              <ColorPicker color={color1} onChange={setColor1} />
+              <ColorPicker color={color2} onChange={setColor2} />
+            </SettingsRow>
 
-              {/* TODO: Decide if "Color Style" is the best label, UX-wise */}
-              <SettingsRow label='Color Style'>
-                <select
-                  className='settings-select'
-                  value={colorStyle}
-                  onChange={(e) => setColorStyle(e.target.value)}
-                >
-                  {COLOR_STYLE_OPTS.map(({ value, text }) => (
-                    <option value={value} key={value}>
-                      {text}
-                    </option>
-                  ))}
-                </select>
-              </SettingsRow>
+            {/* TODO: Decide if "Color Style" is the best label, UX-wise */}
+            <SettingsRow label='Color Style'>
+              <select
+                className='settings-select'
+                value={colorStyle}
+                onChange={(e) => setColorStyle(e.target.value)}
+              >
+                {COLOR_STYLE_OPTS.map(({ value, text }) => (
+                  <option value={value} key={value}>
+                    {text}
+                  </option>
+                ))}
+              </select>
+            </SettingsRow>
 
-              {/* TODO: This a different type of setting, compared to others.
-                  Should be displayed separately? Or somehow differentiated in the UI. */}
-              <SettingsRow label='CPUs'>
-                <select
-                  className='settings-select'
-                  value={numCPUs}
-                  onChange={(e) => setNumCPUs(parseInt(e.target.value, 10))}
-                >
-                  {/* TODO: placeholder values, implement for real */}
-                  {[1, 2, 3, 4, 5, 6, 7, 8].map((value) => (
-                    <option value={value} key={value}>
-                      {value}
-                    </option>
-                  ))}
-                </select>
-              </SettingsRow>
-            </div>
+            {/* TODO: This a different type of setting, compared to others.
+                Should be displayed separately? Or somehow differentiated in the UI. */}
+            <SettingsRow label='CPUs'>
+              <select
+                className='settings-select'
+                value={numCPUs}
+                onChange={(e) => setNumCPUs(parseInt(e.target.value, 10))}
+              >
+                {/* TODO: placeholder values, implement for real */}
+                {[1, 2, 3, 4, 5, 6, 7, 8].map((value) => (
+                  <option value={value} key={value}>
+                    {value}
+                  </option>
+                ))}
+              </select>
+            </SettingsRow>
           </div>
-        )}
-      </SlideOutPanel>
-    </div>
+        </div>
+      )}
+    </SlideOutPanel>
   );
 }
 
@@ -134,15 +136,12 @@ function SlideOutPanel({
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className={`slide-out-panel-container ${isOpen && 'visible'}`}>
-      {isOpen && (
-        <div
-          className='slide-out-panel-content'
-          onMouseLeave={() => setIsOpen(false)}
-        >
-          {children({ isOpen, setIsOpen })}
-        </div>
-      )}
+    <div
+      className={classnames('slide-out-panel-container', isOpen && 'visible')}
+    >
+      <div className='slide-out-panel' onMouseLeave={() => setIsOpen(false)}>
+        {children({ isOpen, setIsOpen })}
+      </div>
       <div
         className='slide-out-panel-opener'
         onMouseEnter={() => setIsOpen(true)}
